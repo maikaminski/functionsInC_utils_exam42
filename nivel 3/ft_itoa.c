b6688ff56014b7677b404c77a0a0af1689009fd9
maikaminski <makamins@c1r5p2.42sp.org.br>
2025-03-13 14:09:45 -0300

    insertions

--- nivel 3/ft_itoa.c
+++ nivel 3/ft_itoa.c
@@ -1,51 +1,43 @@
 #include <stdlib.h> 
 
-static int	ft_get_num_length(int n)
+int	abs_value(int n)
 {
-	int	length;
+	if (n < 0)
+		return (-n);
+	return (n);
+}
 
-	length = 0;
+int	get_len (int n)
+{
+	int len = 0;
 	if (n <= 0)
-		length = 1;
+		++len;
 	while (n != 0)
 	{
-		n /= 10;
-		length++;
+		++len;
+		n = n / 10;
 	}
-	return (length);
+	return (len);
 }
 
-static void	ft_fill_digits(char *str, unsigned int num, int length)
+char *ft_itoa(int n)
 {
-	str[length] = '\0';
-	while (length > 0)
-	{
-		length--;
-		str[length] = '0' + (num % 10);
-		num /= 10;
-	}
-}
+	char *result;
+	int len;
 
-char	*ft_itoa(int nbr)
-{
-	unsigned int	num;
-	char			*result;
-	int				length;
-
-	length = ft_get_num_length(nbr);
-	result = (char *)malloc(length + 1);
-	if (!result)
-		return (NULL);
-	if (nbr < 0)
-	{
+	len = get_len(n);
+	result = malloc(sizeof(char) * (len + 1));
+	result[len] = '\0';
+	if (n < 0)
 		result[0] = '-';
-		num = -nbr;
-		ft_fill_digits(result + 1, num, length - 1);
-	}
-	else
+	else if (n == 0)
+		result[0] = '0';
+	while (n != 0)
 	{
-		num = nbr;
-		ft_fill_digits(result, num, length);
+		--len;
+		result[len] = abs_value(n % 10) + '0';
+		n = n / 10;
 	}
 	return (result);
 }
+
---
+++ nivel 3/split.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define WD_LEN 1000
+
+char **ft_split(char *str)
+{
+	char	**array;
+	int i = 0;
+	int j = 0;
+	int k;
+
+	array = malloc(sizeof(char *) * WD_LEN);
+	while (str[i])
+	{
+		if (str[i] > 32)
+		{
+			k = 0;
+			array[j] = malloc(sizeof(char *) * WD_LEN);
+			while (str[i] > 32)
+				array[j][k++] = str[i++];
+			array[j++][k] = '\0';
+		}
+		else
+			i++;
+	}
+	array[j] = NULL;
+	return (array);
+}
+
+int main(int argc, char **argv)
+{
+	char	**split;
+	int		i;
+
+	if (argc == 2)
+	{
+		split = ft_split(argv[1]);
+			printf("%s ", split[0]);
+		i = 1;
+		while (split[i] != 0)
+		{
+			printf("%s ", split[i]);
+			i++;
+		}
+		printf("%s", split[i]);
+	}
+	printf("\n");
+    return (0);
+}
